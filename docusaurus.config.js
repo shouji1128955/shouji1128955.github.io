d4e4757ae4495250c68dc8182f5fa8ee0335fa6e
shouji1128955 <laiqiang.zhang@aminer.cn>
2026-01-26 02:49:27 +0800

    change_domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://book.onlinebookshell.co.in/',
+  url: 'https://blog.agi-infra.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
